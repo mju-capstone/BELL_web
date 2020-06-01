33a022475a8ee9c187821140dc8f7d261af0572a
Choieunji0114 <cheunji8209@naver.com>
2020-06-02 03:43:49 +0900

    Update googlemap
    get my location & add marker

--- bell_web/public/javascripts/search.js
+++ bell_web/public/javascripts/search.js
@@ -9,13 +9,60 @@ $(document).ready(function(){
     });
 });
 
-// set map properties
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+  }
+
+// 지도 설정
 function initialize() {
-    var mapProp = {
-      center:new google.maps.LatLng(37.250943, 127.028344),
-      zoom:10,
-      mapTypeId:google.maps.MapTypeId.ROADMAP
-    };
-    var map=new google.maps.Map(document.getElementById("map_container"),mapProp);
+
+    // default map
+    map = new google.maps.Map(document.getElementById('map_container'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 15
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+    // get my location
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+        var marker = new google.maps.Marker({
+            position : pos,
+            animation : google.maps.Animation.BOUNCE
+        });
+        marker.setMap(map);
+
+        infoWindow.open(map);
+        map.setCenter(pos);
+    }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+    // var mapProp = {
+    //   center: new google.maps.LatLng(37.580468, 126.923456),
+    //   zoom:15,
+    //   mapTypeId:google.maps.MapTypeId.ROADMAP
+    // };
+    // var map=new google.maps.Map(document.getElementById("map_container"),mapProp); 
+    
+    // var marker = new google.maps.Marker({
+    //     position:{lat: 37.580468, lng: 126.923456},
+    //     animation:google.maps.Animation.BOUNCE
+    // });
+    // marker.setMap(map);
+
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
