d5dd68368e7042fdfc77fd22bb94c241e7da1646
Choieunji0114 <cheunji8209@naver.com>
2020-06-01 21:43:40 +0900

    Setting bootstrap & Create page
    Create aqiInfo.pug, searchRegion.pug

--- bell_web/routes/index.js
+++ bell_web/routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* get aqi page */
+
+router.get('/search', function(req,res,next){
+  res.render('aqi_includes/searchRegion')
+})
+
+router.get('/info', function(req, res, next){
+  res.render('aqi_includes/aqiInfo')
+})
+
+
 module.exports = router;
