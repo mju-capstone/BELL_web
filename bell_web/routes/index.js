b36850e27170c0f7685b10409b636abb19dd80ee
Choieunji0114 <cheunji8209@naver.com>
2020-06-02 14:03:20 +0900

    Install chart.js

--- bell_web/routes/index.js
+++ bell_web/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var Chart = require('chart.js')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
