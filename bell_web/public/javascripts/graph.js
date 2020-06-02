93964bef9dbcfddb4fe0b0b48a2b2c6525ad9f91
Choieunji0114 <cheunji8209@naver.com>
2020-06-02 15:25:34 +0900

    Create Aqi Chart Graph

--- bell_web/public/javascripts/graph.js
+++ bell_web/public/javascripts/graph.js
@@ -1,17 +1,22 @@
 $(document).ready(function(){
-    var ctx = document.getElementById("graph_container").getContext("2d");
+    var ctx = document.getElementById("china_seoul_graph").getContext("2d");
     var chart = new Chart(ctx, {
         // The type of chart we want to create
         type: "line",
         // The data for our dataset
         data: {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
             datasets: [
                 {
-                    label: "Aqi",
-                    // backgroundColor: "rgb(255, 99, 132)",
+                    label: "Korea",
                     borderColor: "rgb(255, 99, 132)",
-                    data: [0, 10, 5, 2, 20, 30, 45],
+                    data: [0, 10, 5, 2, 20, 30, 45, 0, 10, 5, 2, 20, 30],
+                    fill: false
+                },
+                {
+                    label: "China",
+                    borderColor: "rgb(255, 99, 100)",
+                    data: [10, 20, 15, 12, 50, 40, 35, 20, 15, 12, 50, 40, 35],
                     fill: false
                 }
             ]
@@ -24,7 +29,35 @@ $(document).ready(function(){
                 yAxes: [{
                     stacked: true
                 }]
-            }
+            },
         }
-    });             
+    }); 
+    
+    var ctx2 = document.getElementById("status_graph").getContext("2d");
+    var chart2 = new Chart(ctx2, {
+        // The type of chart we want to create
+        type: "line",
+        // The data for our dataset
+        data: {
+            labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+            datasets: [
+                {
+                    label: "Aqi10",
+                    borderColor: "rgb(255, 99, 132)",
+                    data: [0, 10, 5, 2, 20, 30, 45, 0, 10, 5, 2, 20, 30],
+                    fill: false
+                }
+            ]
+        },
+        // Configuration options go here
+        options: {
+            responsive:false,
+            maintainAspectRatio: false,
+            scales: {
+                yAxes: [{
+                    stacked: true
+                }]
+            },
+        }
+    }); 
 });
