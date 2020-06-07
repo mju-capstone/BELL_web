7a85fa98921aa8eaab94af40d478c4f8f2a69f2f
KimBoyoung <33210702+KimBoyoung@users.noreply.github.com>
2020-06-08 00:34:05 +0900

    Create Toggle Button

--- bell_web/public/javascripts/search.js
+++ bell_web/public/javascripts/search.js
@@ -1,3 +1,31 @@
+$(document).ready(function(){
+    $(".year18Button").click(function() {
+        $(".year18").show();
+        $(".year19").hide();
+        $(".year20").hide();
+    });          
+});
+$(document).ready(function(){
+    $(".year19Button").click(function() {
+        $(".year18").hide();
+        $(".year19").show();
+        $(".year19").css({
+            'visibility': 'visible'
+        });
+        $(".year20").hide();
+    });          
+});
+$(document).ready(function(){
+    $(".year20Button").click(function() {
+        $(".year18").hide();
+        $(".year19").hide();
+        $(".year20").show();
+        $(".year20").css({
+            'visibility': 'visible'
+        });
+    });          
+});
+
 // 검색하기 버튼 클릭 이벤트
 $(document).ready(function(){
     $(".searchButton").click(function() {
