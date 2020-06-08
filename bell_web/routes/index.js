8e454a85cfd8046cf7ad50762d09e15abd7035c1
jiwoo <56306637+lee-jiu@users.noreply.github.com>
2020-06-09 03:25:30 +0900

    Modify api_info view

--- bell_web/routes/index.js
+++ bell_web/routes/index.js
@@ -22,7 +22,7 @@ const addr3 = '&ver=1.3&_returnType=json'
 var myaddr = addr + encodeURI('서대문구') + addr2 + key + addr3
 
 router.get('/aqi', catchErrors(async (req, res, next) => {
-  var data, pm10, cai, o3, no2, so2, co, pm10Grade1h, pm10Grade, caiGrade, so2Grade, coGrade, o3Grade, no2Grade
+  var data, pm10, cai, o3, no2, so2, co, pm10Grade1h, pm10Grade, caiGrade, so2Grade, coGrade, o3Grade, no2Grade, pm25, pm25Grade1h
 
   request(myaddr, function(error, response, body){
     if(error){
@@ -45,8 +45,11 @@ router.get('/aqi', catchErrors(async (req, res, next) => {
     coGrade = obj.list[0].coGrade
     o3Grade = obj.list[0].o3Grade
     no2Grade = obj.list[0].no2Grade
+    pm25 = obj.list[0].pm25Value
+    pm25Grade1h = obj.list[0].pm25Grade1h
 
-    res.render('aqi/aqi_page', {data: data, cai:cai, pm10:pm10, o3:o3, so2:so2, co:co, no2:no2, caiGrade:caiGrade, pm10Grade1h:pm10Grade1h, so2Grade:so2Grade, coGrade:coGrade, o3Grade:o3Grade, no2Grade:no2Grade})
+    res.render('aqi/aqi_page', {data: data, cai:cai, pm10:pm10, o3:o3, so2:so2, co:co, no2:no2, caiGrade:caiGrade, pm10Grade1h:pm10Grade1h,
+       so2Grade:so2Grade, coGrade:coGrade, o3Grade:o3Grade, no2Grade:no2Grade, pm25:pm25, pm25Grade1h:pm25Grade1h})
   });
 }))
 
