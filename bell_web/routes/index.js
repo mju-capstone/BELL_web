733510bc94ca8917555b1c83b5f0750a5da57716
Choieunji0114 <cheunji8209@naver.com>
2020-06-10 21:48:31 +0900

    Connect to MySQL

--- bell_web/routes/index.js
+++ bell_web/routes/index.js
@@ -4,6 +4,16 @@ var request = require('request');
 
 const catchErrors = require('../lib/async-error');
 
+// MARK : Connect to MySQL
+const mysql =  require('mysql')
+
+const connection  = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'testDB'
+})
+
 var Chart = require('chart.js')
 
 /* GET home page. */
@@ -13,15 +23,26 @@ router.get('/', function(req, res, next) {
 
 
 // call api for finedust
+const key = 'g5wuVXrLzJMBI9kR2gmdXm6ltsn0zYEicoOG7g2xNHZnGZVp9v7znsIO45M2l7R6rlE5wiD%2FjtIZupMYvyN2Pg%3D%3D'
+
+const addr = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName='
+const addr2 = '&dataTerm=month&pageNo=1&numOfRows=10&ServiceKey=' 
+const addr3 = '&ver=1.3&_returnType=json'
+
+var myaddr = addr + encodeURI('서대문구') + addr2 + key + addr3
+
 router.get('/aqi', catchErrors(async (req, res, next) => {
-  const key = 'g5wuVXrLzJMBI9kR2gmdXm6ltsn0zYEicoOG7g2xNHZnGZVp9v7znsIO45M2l7R6rlE5wiD%2FjtIZupMYvyN2Pg%3D%3D'
 
-  const addr = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName='
-  const addr2 = '&dataTerm=month&pageNo=1&numOfRows=10&ServiceKey=' 
-  const addr3 = '&ver=1.3&_returnType=json'
-
-  var myaddr = addr + encodeURI('서대문구') + addr2 + key + addr3
-  // var myaddr = addr + encodeURI(res.body.class) + addr2 + key + addr3
+  // 일단 DB연결부분..주석
+  // var queryString = "SELECT * FROM user"
+  // let result1
+  // connection.query(queryString, (err,rows,fields) => {
+  //   console.log(rows)
+  //   result1 = rows[0].first_name
+  // })
+  // setTimeout(function(){ 
+  //   console.log(result1)
+  // }, 1000);
 
   var data, pm10, cai, o3, no2, so2, co, pm10Grade1h, pm10Grade, caiGrade, so2Grade, coGrade, o3Grade, no2Grade, pm25, pm25Grade1h
 
@@ -30,8 +51,6 @@ router.get('/aqi', catchErrors(async (req, res, next) => {
       console.log(error)
     }
     var obj = JSON.parse(body)
-    console.log(obj.list[0])
-
     data = obj.list[0].dataTime
     pm10 = obj.list[0].pm10Value
     cai = obj.list[0].khaiValue
@@ -48,10 +67,23 @@ router.get('/aqi', catchErrors(async (req, res, next) => {
     no2Grade = obj.list[0].no2Grade
     pm25 = obj.list[0].pm25Value
     pm25Grade1h = obj.list[0].pm25Grade1h
-
-    res.render('aqi/aqi_page', {data: data, cai:cai, pm10:pm10, o3:o3, so2:so2, co:co, no2:no2, caiGrade:caiGrade, pm10Grade1h:pm10Grade1h,
-       so2Grade:so2Grade, coGrade:coGrade, o3Grade:o3Grade, no2Grade:no2Grade, pm25:pm25, pm25Grade1h:pm25Grade1h})
   });
+
+  setTimeout(function(){ 
+    res.render('aqi/aqi_page', {data: data, cai:cai, pm10:pm10, o3:o3, so2:so2, co:co, no2:no2, caiGrade:caiGrade, pm10Grade1h:pm10Grade1h,
+      so2Grade:so2Grade, coGrade:coGrade, o3Grade:o3Grade, no2Grade:no2Grade, pm25:pm25, pm25Grade1h:pm25Grade1h})
+  }, 500);
+
+  setTimeout(function(){
+
+  })
+
 }))
 
+// MARK : 검색
+// router.post('/aqi/search', catchErrors(async (req, res, next) => {
+
+// }))
+
 module.exports = router;
+
