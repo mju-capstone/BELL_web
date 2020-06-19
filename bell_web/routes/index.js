161ed408dae3d090f98e008e15f6b3655a05257d
Choieunji0114 <cheunji8209@naver.com>
2020-06-19 22:57:59 +0900

    Update weather API

--- bell_web/routes/index.js
+++ bell_web/routes/index.js
@@ -43,32 +43,25 @@ const addr = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInq
 const addr2 = '&dataTerm=month&pageNo=1&numOfRows=10&ServiceKey=' 
 const addr3 = '&ver=1.3&_returnType=json'
 
+const weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=853c909f0f8639d63344ec1b9f73c12a'
+
 router.get('/aqi', catchErrors(async (req, res, next) => {
-
-  // 일단 DB연결부분..주석
-  // var queryString = "SELECT * FROM finedust2018"
-  // let result1
-  // connection.query(queryString, (err,rows,fields) => {
-  //   console.log(rows)
-  //   result1 = rows
-  // })
-  // setTimeout(function(){ 
-  //   console.log(result1)
-  // }, 1000);
-
-  var data, pm10, cai, o3, no2, so2, co, pm10Grade1h, pm10Grade, caiGrade, so2Grade, coGrade, o3Grade, no2Grade, pm25, pm25Grade1h
-
+  var data, pm10, cai, o3, no2, so2, co, pm10Grade1h, pm10Grade, caiGrade, so2Grade, coGrade, o3Grade, no2Grade, pm25, pm25Grade1h, mytemp
   var myaddr = addr + encodeURI('강남구') + addr2 + key + addr3
 
-  var weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=853c909f0f8639d63344ec1b9f73c12a"
+  // MARK : 1. call weather API 
   request(weatherAPI, function(error, response, body){
     if(error){
       console.log(error)
     }
     var weatherOBJ = JSON.parse(body)
-    console.log(weatherOBJ)
+    var temp = weatherOBJ.main.temp
+    var newtemp = temp-273.15 // kelvin to celsius 
+    mytemp = Math.floor(newtemp)
+    console.log(mytemp)
   })
 
+  // MARK : 2. call fine dust API 
   request(myaddr, function(error, response, body){
     if(error){
       console.log(error)
@@ -94,10 +87,9 @@ router.get('/aqi', catchErrors(async (req, res, next) => {
 
   setTimeout(function(){ 
     res.render('aqi/aqi_page', {data: data, cai:cai, pm10:pm10, o3:o3, so2:so2, co:co, no2:no2, caiGrade:caiGrade, pm10Grade1h:pm10Grade1h,
-      so2Grade:so2Grade, coGrade:coGrade, o3Grade:o3Grade, no2Grade:no2Grade, pm25:pm25, pm25Grade1h:pm25Grade1h})
-  }, 500);
+      so2Grade:so2Grade, coGrade:coGrade, o3Grade:o3Grade, no2Grade:no2Grade, pm25:pm25, pm25Grade1h:pm25Grade1h, mytemp:mytemp})
+  }, 1500);
   
-
 }))
 
 router.post('/search', catchErrors(async (req, res, next)=> {
