5a7a934dd4f5934a75e3e4b2b55f051244ba80ae
limhyejin <hejin8307@naver.com>
2020-06-20 17:59:07 +0900

    trying to google map

--- bell_web/public/javascripts/search.js
+++ bell_web/public/javascripts/search.js
@@ -125,4 +125,34 @@ function initialize() {
 
 
 
-google.maps.event.addDomListener(window, 'load', initialize);
+// window.initMap = function() {
+//     var map = new google.maps.Map(document.getElementById('map'), {
+//         zoom: 8,
+//         center: {lat: -34.397, lng: 150.644}
+//     });
+//     var geocoder = new google.maps.Geocoder();
+
+//     document.getElementById('submit').addEventListener('click', function() {
+//         geocodeAddress(geocoder, map);
+//     });
+// }
+
+// function geocodeAddress(geocoder, resultsMap) {
+//     var address = document.getElementById('address').value;
+//     geocoder.geocode({'address': address}, function(results, status) {
+//         if (status === 'OK') {
+//             resultsMap.setCenter(results[0].geometry.location);
+//             var marker = new google.maps.Marker({
+//             map: resultsMap,
+//             position: results[0].geometry.location
+//             });
+//         } else {
+//             alert('Geocode was not successful for the following reason: ' + status);
+//         }
+//     });
+// }
+
+
+
+
+// google.maps.event.addDomListener(window, 'load', initialize);
